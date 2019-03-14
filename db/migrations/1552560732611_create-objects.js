7be4c845406a865025d2139c0759bb31b6d2af03
andypf <andreas.pfau@sap.com>
2019-03-14 13:53:12 +0000

    add db migrations

---
+++ db/migrations/1552560732611_create-objects.js
@@ -0,0 +1,19 @@
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable("objects", {
+    id: { type: "varchar(500)", notNull: true, unique: true },
+    name: { type: "varchar(1000)"},
+    object_type: { type: "varchar(500)", notNull: true },
+    payload: { type: 'jsonb' }
+  })
+
+  pgm.createIndex("objects", "id")
+  pgm.createIndex("objects", "object_type")
+
+  // add more indices on payload for keys wich are used in where clausels
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable("objects", { ifExists: true })
+};
