668dc5df613353b0395c94814a58b5b986e2ffce
andypf <andreas.pfau@sap.com>
2019-03-19 10:55:09 +0000

    update swagger definition

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,42 @@
+/**
+ * @swagger
+ * components:
+ *   responses:
+ *     BadRequest:
+ *       description: Bad request
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Error'
+ *     UnexpectedError:
+ *       description: Unexpected error
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Error'
+ *     NotFound:
+ *       description: The specified resource was not found
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Error'
+ *     Unauthorized:
+ *       description: Unauthorized
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Error'
+ *   schemas:
+ *     # Schema for error response body
+ *     Error:
+ *       type: object
+ *       properties:
+ *         code:
+ *           type: string
+ *         message:
+ *           type: string
+ */
+
 const objects = require('./objects/objects.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
--- src/services/objects/objects.class.js
+++ src/services/objects/objects.class.js
@@ -3,24 +3,44 @@ const { Pool } = require('pg')
 
 /**
  * @swagger
- * definitions:
- *   Object:
- *     type: object
- *     required: 
- *       - id
- *       - name
- *       - object_type
- *       - payload
- *     properties:
- *       id:
- *         type: string
- *       name: 
- *         type: string
- *       object_type:
- *         type: string
- *       payload:
- *         type: string
- *         description: a valid **JSON** string
+ * components:
+ *   schemas:
+ *     Object:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *         name: 
+ *           type: string
+ *         object_type:
+ *           type: string
+ *         created_at: 
+ *           type: string
+ *           format: date-time
+ *         updated_at:
+ *           type: string
+ *           format: date-time
+ *         payload:
+ *           type: string
+ *           description: a valid **JSON** string
+ *
+ *     Object New:
+ *       type: object
+ *       required: 
+ *         - id
+ *         - name
+ *         - object_type
+ *         - payload
+ *       properties:  
+ *         id:
+ *           type: string
+ *         name: 
+ *           type: string
+ *         object_type:
+ *           type: string
+ *         payload:
+ *           type: string
+ *           description: a valid **JSON** string
  */
 module.exports = class Service {
   constructor (options) {
@@ -30,18 +50,52 @@ module.exports = class Service {
 
   /**
    * @swagger
-   * /objects/{id}:
+   * /objects:
    *   get:
-   *     description: Returns a specific object
+   *     description: Retrieve a list of objects
+   *     summary: GET /objects
    *     tags:
    *       - object
    *     produces:
    *       - application/json
+   *     parameters:
+   *       - in: query
+   *         name: searchTerm
+   *         description: Term for the full-text search   
+   *         schema:
+   *           type: string
+   *           example: '?searchTerm=test'
+   *       - in: query
+   *         name: idIn
+   *         description: comma separeted list of object ids to retrieve corresponding objects 
+   *         schema:
+   *           type: string        
+   *           example: '?idIn=1,2,3,4,5'
+   *       - in: query
+   *         name: typeIn
+   *         description: comma separeted list of object types to retrieve corresponding objects 
+   *         schema:
+   *           type: string        
+   *           example: '?typeIn=server,ip'
+   *       - in: query
+   *         name: where
+   *         description: 'key,value. Multiple Where clauses are combined with AND'
+   *         schema:
+   *           type: string
+   *           example: '?where=network_id,1'
    *     responses: 
    *       200:
-   *         description: objects
-   *         schema:
-   *           $ref: '#/definitions/Object'
+   *         description: Success
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: array
+   *               items:
+   *                 $ref: '#/components/schemas/Object'
+   *       400:
+   *         $ref: '#/components/responses/BadRequest'
+   *       500:
+   *         $ref: '#/components/responses/UnexpectedError'
    */
   async find (params) {
     const client = await this.pool.connect()
@@ -53,13 +107,61 @@ module.exports = class Service {
       return e //console.error(e.stack)
     }
   }
-
+ /**
+   * @swagger
+   * /objects/{id}:
+   *   get:
+   *     description: Retrieve a specific object
+   *     summary: GET /objects/{id}
+   *     tags:
+   *       - object
+   *     produces:
+   *       - application/json
+   *     responses: 
+   *       200:
+   *         description: objects list
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: '#/components/schemas/Object' 
+   *       400:
+   *         $ref: '#/components/responses/BadRequest'
+   *       404:
+   *         $ref: '#/components/responses/NotFound'
+   *       500:
+   *         $ref: '#/components/responses/UnexpectedError'
+   */
   async get (id, params) {
     return {
       id, text: `A new message with ID: ${id}!`
     };
   }
 
+ /**
+   * @swagger
+   * /objects:
+   *   post:
+   *     description: Add or update an object
+   *     summary: POST /objects
+   *     tags:
+   *       - object 
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             $ref: '#/components/schemas/Object New'
+   *     produces:
+   *       - application/json
+   *     responses: 
+   *       201:
+   *         description: Created
+   *       202:
+   *         description: Updated
+   *       500:
+   *         $ref: '#/components/responses/UnexpectedError'
+
+   */
   async create (data, params) {
     const text = 'INSERT INTO objects(id, name, object_type, payload) VALUES($1, $2, $3, $4) RETURNING *'
     const client = await this.pool.connect()
